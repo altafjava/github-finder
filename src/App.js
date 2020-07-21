d2ee41ff58b6de2bef0224b2999307becdab7db8
Altaf <samaltaf6@gmail.com>
2020-07-21 19:41:15 +0530

    using Fragment instead of div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, {Fragment, Component } from 'react';
 import './App.css';
 
 class App extends Component{
   render() {
     return (
-      <div className='App'>
+      <Fragment>
         <h1>Hello from React</h1>
-      </div>
+      </Fragment>
     );
   }
 }
