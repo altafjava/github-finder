d0f8ebb1a8194d987ef9bf377e43534b995c208b
Altaf <samaltaf6@gmail.com>
2020-07-22 07:39:20 +0530

    multi users displaying

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
       </Fragment>
     );
   }
