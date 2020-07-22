bfe28bee124ea0abb6ddf33e7dc01fb33dd6d625
Altaf <samaltaf6@gmail.com>
2020-07-22 10:18:09 +0530

    fetching github users through axios

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const response = await Axios.get('https://api.github.com/users');
+    this.setState({ loading: false, users: response.data });
+  }
+
   render() {
     return (
       <Fragment>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </Fragment>
     );
