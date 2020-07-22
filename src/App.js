954aeaa1d499320e9df379842d24fe2249af92bd
Altaf <samaltaf6@gmail.com>
2020-07-22 18:29:23 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar';
 import Search from './components/users/Search';
 import Users from './components/users/Users';
 import Alert from './components/layout/Alert';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   state = {
@@ -38,19 +39,31 @@ class App extends Component {
   render() {
     const { loading, users } = this.state;
     return (
-      <Fragment>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={this.state.users} />
-        </div>
-      </Fragment>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={this.state.users} />
+                  </Fragment>
+                )}
+              />
+            </Switch>
+          </div>
+        </Fragment>
+      </BrowserRouter>
     );
   }
 }
