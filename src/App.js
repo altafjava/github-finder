6a7aba6caad0bde3d464df4b9fa329213c469fe7
Altaf <samaltaf6@gmail.com>
2020-07-23 17:51:59 +0530

    Cleanup: Home & NotFound created

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import User from './components/users/User';
-import Users from './components/users/Users';
 import AlertState from './context/alert/AlertState';
 import GithubState from './context/github/GithubState';
+import NotFound from './components/pages/NotFound';
 
 const App = () => {
   // useEffect(() => {
@@ -30,18 +30,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={() => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </Fragment>
